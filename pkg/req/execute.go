9b7705b50b904ceadfbbb8c5619241ddead4297f
RojetSavage <robert-hurley@hotmail.com.au>
2024-02-06 20:39:14 +1100

    implement url user/password

--- pkg/req/execute.go
+++ pkg/req/execute.go
@@ -6,14 +6,14 @@ import (
 	"strings"
 )
 
-func ApplyFlagsToRequest(r *Request, flags []args.UserFlag) error {
+func ApplyFlagsToRequest(r *RequestHandler, flags []args.UserFlag) error {
 	for _, f := range flags {
 		applyFlagToRequest(r, f)
 	}
 	return nil
 }
 
-func applyFlagToRequest(r *Request, f args.UserFlag) error {
+func applyFlagToRequest(r *RequestHandler, f args.UserFlag) error {
 	var err error
 
 	switch f.F {
@@ -21,6 +21,8 @@ func applyFlagToRequest(r *Request, f args.UserFlag) error {
 		r.resetRequest()
 	case "s", "scheme":
 		err = r.changeUri("scheme", f.Parameter)
+	case "u", "user":
+		err = r.changeUri("user", f.Parameter)
 	case "host":
 		err = r.changeUri("host", f.Parameter)
 	case "P", "port":
@@ -47,18 +49,18 @@ func applyFlagToRequest(r *Request, f args.UserFlag) error {
 		r.setHttpMethod(http.MethodPatch)
 	case "trace":
 		r.setHttpMethod(http.MethodTrace)
-
+	case "h", "header":
+		r.addHeader(f.Parameter)
 	case "d", "data":
 		err = r.changeUri("query", f.Parameter)
-	case "j", "json":
-		r.addHeader(strings.Join([]string{"Content-Type", "application/json"}, "="))
-		r.setHttpMethod(http.MethodPost)
-		r.changeRequestBody(f.Parameter)
-
 	case "c", "contentType":
 		r.addHeader(strings.Join([]string{"Content-Type", f.Parameter}, "="))
 	case "C", "cookie":
 		r.addCookieToReq(f.Parameter)
+	case "j", "json":
+		r.addHeader(strings.Join([]string{"Content-Type", "application/json"}, "="))
+		r.setHttpMethod(http.MethodPost)
+		r.changeRequestBody(f.Parameter)
 	}
 
 	if err != nil {
@@ -66,4 +68,22 @@ func applyFlagToRequest(r *Request, f args.UserFlag) error {
 	}
 
 	return nil
+}
+
+func ApplyFlagsToResponse(r *ResponseHandler, flags []args.UserFlag) error {
+	for _, f := range flags {
+		applyFlagToResponse(r, f)
+	}
+	return nil
+}
+
+func applyFlagToResponse(r *ResponseHandler, f args.UserFlag) error {
+	var err error
+
+	switch f.F {
+	case "D", "dump-header":
+		r.dumpHeader(f.Parameter)
+	}
+
+	return err
 }
